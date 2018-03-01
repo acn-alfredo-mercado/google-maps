a1d67bdde0a5661f3793f97235e47c88921d11c5
Mercado, Alfredo I. R <alfredo.i.r.mercado@accenture.com>
2018-03-01 14:56:21 +0800

    Added inital state of location

--- src/Map/MapContainer.jsx
+++ src/Map/MapContainer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './MapContainer.css';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class MapContainer extends Component {
   constructor(props) {
@@ -35,16 +35,38 @@ class MapContainer extends Component {
   render() {
     return (
       <Map google={this.props.google}
-          onClick={this.onMapClicked}>
+        onClick={this.onMapClicked}
+        zoom={12}
+        initialCenter={{
+          lat: 1.294143,
+          lng: 103.853278
+        }}>
         <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
+          name={'pre'} />
+
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
+          <div class="row">
+            <div class="col-7">
+              <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                  <div>
+                    <h4>{this.state.selectedPlace.name}</h4>
+                    <div class="content">
+                      <img src={this.picture} />
+                      <span>{this.name}</span><br />
+                      <span>{this.disease}, {this.locationName}</span>
+                    </div>
+                  </div>
+                  <hr />
+                  <p>Details</p>
+                </div>
+                <hr />
+                <p>battery status</p>
+              </div>
             </div>
+          </div>
         </InfoWindow>
       </Map>
     )
